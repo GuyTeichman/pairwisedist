22ccacc304cbca664e529e324e9c466c89e30f22
GuyTeichman <48219633+GuyTeichman@users.noreply.github.com>
2020-06-17 23:57:48 +0300

    updated version #

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/GuyTeichman/pairwisedist',
-    version='0.0.1',
+    version='1.0.0',
     zip_safe=False,
 )
