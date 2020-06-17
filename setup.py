d093b3582efc36603a1d2e4a69115112fa2fa3af
GuyTeichman <48219633+GuyTeichman@users.noreply.github.com>
2020-06-17 21:58:01 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = ['numpy', 'scipy']
+
+setup_requirements = ['pytest-runner', ]
+
+test_requirements = ['pytest', ]
+
+setup(
+    author="Guy Teichman",
+    author_email='guyteichman@gmail.com',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Visualization',
+    ],
+    description='',
+    install_requires=requirements,
+    python_requires='>3.6',
+    license="License :: OSI Approved :: Apache Software License",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords=['distance', 'pairwise distance', 'YS1', 'YR1', 'pairwise-distance matrix', 'Son and Baek dissimilarities',
+              'Son and Baek'],
+    name='pairwisedist',
+    packages=find_packages(),
+    # packages=find_packages(include=['pairwisedist']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/GuyTeichman/pairwisedist',
+    version='0.0.1',
+    zip_safe=False,
+)
