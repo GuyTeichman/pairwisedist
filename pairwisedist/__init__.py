e8a4f48dde04dbca49c382654f9dcbe178400375
GuyTeichman <48219633+GuyTeichman@users.noreply.github.com>
2022-12-27 22:33:47 +0200

    Bump version: 1.2.0 → 1.3.0

--- pairwisedist/__init__.py
+++ pairwisedist/__init__.py
@@ -3,6 +3,6 @@ from pairwisedist.pairwisedist import *
 __name__ = "pairwisedist"
 __author__ = """Guy Teichman"""
 __email__ = "guyteichman@gmail.com"
-__version__ = "1.2.0"
+__version__ = "1.3.0"
 __license__ = "Apache"
 
