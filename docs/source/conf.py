33cb6c4acfc29b73fad9efbc2f13d4219c045ffc
GuyTeichman <48219633+GuyTeichman@users.noreply.github.com>
2023-01-13 19:47:04 +0200

    Bump version: 1.3.0 → 1.3.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, Guy Teichman'
 author = 'Guy Teichman'
 
 # The full version, including alpha/beta/rc tags
-release = '1.3.0'
+release = '1.3.1'
 
 # -- General configuration ---------------------------------------------------
 
